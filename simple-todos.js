b3e1d8861b6032c042d2541a144c1f278c642289
Rodrigo Morais <morais.rm@gmail.com>
2014-11-17 22:30:17 -0200

    Show dynamically data from MongoDB and allow inserting new task in list.

--- simple-todos.js
+++ simple-todos.js
@@ -1,9 +1,26 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   Template.body.helpers({
-    tasks: [
-      { text: "This is task 1" },
-      { text: "This is task 2" },
-      { text: "This is task 3" }
-    ]
+    tasks: function () {
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
   });
+
+
+  Template.body.events({
+  "submit .new-task": function (event) {
+
+    var text = event.target.text.value;
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date()
+    });
+
+    event.target.text.value = "";
+
+    return false;
+  }
+});
 }
