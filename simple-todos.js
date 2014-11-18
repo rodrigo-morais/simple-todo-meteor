66302a2410943b52e56ac438cbe4fa9ab3e3f2b9
Rodrigo Morais <morais.rm@gmail.com>
2014-11-18 00:02:20 -0200

    Add login control.

--- simple-todos.js
+++ simple-todos.js
@@ -26,7 +26,9 @@ if (Meteor.isClient) {
 
       Tasks.insert({
         text: text,
-        createdAt: new Date()
+        createdAt: new Date(),
+        owner: Meteor.userId(),
+        username: Meteor.user().username
       });
 
       event.target.text.value = "";
@@ -45,5 +47,9 @@ if (Meteor.isClient) {
     "change .hide-completed input": function (event) {
       Session.set("hideCompleted", event.target.checked);
     }
+  });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
   });
 }
